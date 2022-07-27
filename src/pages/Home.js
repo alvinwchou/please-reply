ac951874d77b1cc51d316ec9b624ce842d11a67a
Alvin Chou <alvinchou516@hotmail.com>
2022-07-27 16:48:21 -0400

    style: :lipstick: general styling - positioning

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,10 +7,10 @@ function Home({ displayName }) {
             <h1>Please Reply</h1>
             <p>A simple app that allows users to create events and keeps trick of people that RSVP</p>
             {!displayName && 
-                <>
+                <div>
                     <Link to='/register'>Register</Link>
                     <Link to='/login'>Log In</Link>
-                </>
+                </div>
             }
         </div>
     )
