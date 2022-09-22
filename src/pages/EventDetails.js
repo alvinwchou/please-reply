81c3b8ba2678856cfd643898372683c4df75c062
Alvin Chou <alvinchou516@hotmail.com>
2022-09-22 19:33:47 -0400

    style: :lipstick: updated styling for eventDetails page

--- src/pages/EventDetails.js
+++ src/pages/EventDetails.js
@@ -1,31 +1,35 @@
 // EventDetails.js
 
 import { getDatabase, ref, remove } from "firebase/database"
-import { Link, useParams } from "react-router-dom"
+import { Link, useNavigate, useParams } from "react-router-dom"
 import firebase from "../firebase"
 
 function EventDetails({ events, userID }) {
     const { eventID: eventID } = useParams();
+
+    const navigate = useNavigate()
 
     const deleteEvent = (whichEvent) => {
         const database = getDatabase(firebase);
         const dbRef = ref(database, `events/${userID}/${whichEvent}`)
 
         remove(dbRef);
+
+        navigate('/events')
     }
 
     return (
         <div className="eventDetails">
-            <h1>Event Details</h1>
+
             {events && events.filter(event => event.eventID == eventID).map(event => {
                 return (
                     <div className="eventCard" key={event.eventID}>
                         <div className="headingTextContainer">
                             <div className="wrapper">
-                                <h3>{new Date(`'${event.startDate}'`).toDateString()} AT {event.startTime} - {new Date(`'${event.endDate}'`).toDateString()} AT {event.endTime}</h3>
-                                
+                                            <h1>Event Details</h1>
                                 <h2>{event.eventName}</h2>
-                                <h3>{event.location}</h3>
+                                <h3>{new Date(`'${event.startDate}'`).toDateString()} {event.startTime && `AT ${event.startTime}`} {(event.endDate || event.endTime) && '-'} {event.endDate && new Date(`'${event.endDate}'`).toDateString()} {event.endDate && 'AT'} {event.endTime && `${event.endTime}`}</h3>
+                                <h3><span className="grey">{event.location}</span></h3>
                                 <div className="eventDetailsOptions">
                                     <Link to={`/signUp/${userID}/${event.eventID}`} className="btn" >Going</Link>
                                     <button className="btn">Invite</button>
@@ -40,7 +44,7 @@ function EventDetails({ events, userID }) {
                             <p>4 people responded <Link to={`/guestList/${userID}/${event.eventID}`}>Guest List</Link></p>
                             <p>Event Created by <span className="bold">{userID}</span></p>
                             <p><span className="bold">{event.location}</span></p>
-                            <p>{event.description ? event.description : 'No details yet'}</p>
+                            {event.description ? <p>{event.description}</p> : <p><span className="grey">No details yet</span></p>}
                         </div>
                     </div>
                 )
