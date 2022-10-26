02663eda39fe1ca7f1021af5739bb37b5ab3ad46
Alvin Chou <alvinchou516@hotmail.com>
2022-10-26 12:53:54 -0400

    docs: :memo: Added a demo account and event to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,30 @@
 // Home.js
 import { Link } from "react-router-dom"
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 function Home({ displayName }) {
+    let navigate = useNavigate()
+
+    const handelClickDemoAccount = () => {
+        const loginInfo = {
+            email: 'alvinchou516@gmail.com',
+            password: 'asdfasdf',
+        }
+
+        signInWithEmailAndPassword(
+            auth,
+            loginInfo.email,
+            loginInfo.password
+        ).then(() => {
+            // route to events page after successful log in
+            navigate('/events');
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
     return (
         <div className="home">
             <h1>Please Reply</h1>
@@ -12,6 +35,10 @@ function Home({ displayName }) {
                     <Link to='/login' className="btn">Log In</Link>
                 </div>
             }
+            <div className="demo">
+                <a href='https://pleasereply.netlify.app/eventDetails/wqBNP4ZQl6d6fM339idyx6XJLyj1/-NEJ7pjKp5QLxH2IZtvp' className="btn">Demo Event</a>
+                <button className="btn" onClick={handelClickDemoAccount}>Demo Account</button>
+            </div>
         </div>
     )
 }
